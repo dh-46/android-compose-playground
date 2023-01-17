bc1150bcc4c8242f56c3f66bffa9de35685988f4
Daniel (EMCT) <daniel.h@emct.com.tw>
2023-01-17 18:10:13 +0800

    Feat: Use Modifiers to define margins and appearances
    https://developer.android.com/jetpack/compose/tutorial

--- app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dh46.compose_tutorial.data.Message
-import com.dh46.compose_tutorial.ui.theme.ComposetutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +26,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.img_sample),
-            contentDescription = "Contact profile picture"
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // set ImageSize to 40 dp
+                .size(40.dp)
+                // Clipped image to be shaped as a circle
+                .clip(CircleShape)
         )
+
+        // Add vertical space to Image and Texts (like margin)
+        Spacer(modifier = Modifier.width(8.dp))
+        
         Column {
             Text(text = msg.author)
+            // Add horizontal space to Author text and body text
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
