049be421ad9105625a9e9d7cb47a35920b45e3b3
Daniel (EMCT) <daniel.h@emct.com.tw>
2023-01-17 18:30:01 +0800

    Feat: Apply Material Design
    https://developer.android.com/jetpack/compose/tutorial

--- app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,12 +17,17 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dh46.compose_tutorial.data.Message
+import com.dh46.compose_tutorial.ui.theme.ComposetutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(msg = Message("Biden", "I'm the President of United States."))
+            ComposetutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(msg = Message("Biden", "I'm the President of United States."))
+                }
+            }
         }
     }
 }
@@ -35,16 +43,28 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // Clipped image to be shaped as a circle
                 .clip(CircleShape)
+                // Add border to Image
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         // Add vertical space to Image and Texts (like margin)
         Spacer(modifier = Modifier.width(8.dp))
-        
+
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             // Add horizontal space to Author text and body text
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
@@ -52,5 +72,9 @@ fun MessageCard(msg: Message) {
 @Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("Biden", "I'm the President of United States."))
+    ComposetutorialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(msg = Message("Biden", "I'm the President of United States."))
+        }
+    }
 }
