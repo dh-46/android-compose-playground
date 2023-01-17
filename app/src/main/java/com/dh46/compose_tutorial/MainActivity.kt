5bac392a8189ef4e0ffc080a88c69444cd4444b7
Daniel (EMCT) <daniel.h@emct.com.tw>
2023-01-17 17:55:16 +0800

    Feat: Use Column and Row to build layout
    https://developer.android.com/jetpack/compose/tutorial

--- app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
@@ -3,53 +3,45 @@ package com.dh46.compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.dh46.compose_tutorial.data.Message
 import com.dh46.compose_tutorial.ui.theme.ComposetutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Biden")
-//            ComposetutorialTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
+            MessageCard(msg = Message("Biden", "I'm the President of United States."))
         }
     }
 }
 
-//@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Hello $name!")
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    ComposetutorialTheme {
-//        Greeting("Android")
-//    }
-//}
-
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "This is a message card which says Hello $name!")
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.img_sample),
+            contentDescription = "Contact profile picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "Testing")
+    MessageCard(Message("Biden", "I'm the President of United States."))
 }
---
+++ app/src/main/java/com/dh46/compose_tutorial/data/Message.kt
@@ -0,0 +1,6 @@
+package com.dh46.compose_tutorial.data
+
+/**
+ *  Created by DanielHuang on 2023/1/17
+ */
+data class Message(val author: String, val body: String)
