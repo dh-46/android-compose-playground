6558739a4aa3e00bbd355a86cadc68fc37f94dd6
Daniel (EMCT) <daniel.h@emct.com.tw>
2023-01-18 14:09:24 +0800

    Feat: Add animations to list item
    https://developer.android.com/jetpack/compose/tutorial

--- app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/dh46/compose_tutorial/MainActivity.kt
@@ -3,8 +3,11 @@ package com.dh46.compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,7 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -54,7 +57,17 @@ fun MessageCard(msg: Message) {
         // Add vertical space to Image and Texts (like margin)
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // Keep track if the message is expanded or not in this variable
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -62,10 +75,20 @@ fun MessageCard(msg: Message) {
             )
             // Add horizontal space to Author text and body text
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // set surfaceColor here it will change gradually
+                color = surfaceColor,
+                // animateContentSize will change padding gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
